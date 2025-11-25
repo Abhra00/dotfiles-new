a115281fa1358ecf80671ae3880fe7d9b24232dc
Abhra00 <abhramondal9674@gmail.com>
2025-11-25 21:50:06 +0530

    Changed default browser

--- qtile/config.py
+++ qtile/config.py
@@ -17,7 +17,7 @@ import colors
 mod         = "mod4"                       # Sets mod key to SUPER/WINDOWS
 alt         = "mod1"                       # Sets the alt key to left-alt key
 myTerm      = "kitty"                      # My terminal of choice
-myBrowser   = "firefox"                    # My browser of choice
+myBrowser   = "chromium"                   # My browser of choice
 myEditor    = "emacsclient -c -a 'emacs'"  # My editor of choice
 myLauncher  = "rofi -show drun"            # My launcher of choice
 

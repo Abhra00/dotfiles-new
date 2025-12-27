dd7d9b8813ae0e3b6c75929d50ae8762f2c20abe
Abhra00 <abhramondal9674@gmail.com>
2025-12-28 03:26:18 +0530

    Moved to solarized osaka

--- .bashrc
+++ .bashrc
@@ -1,12 +1,7 @@
-#  ┳┓┏┓┏┓┓┏┳┓┏┓
-#  ┣┫┣┫┗┓┣┫┣┫┃
-#  ┻┛┛┗┗┛┛┗┛┗┗┛
+#    ┳┓┏┓┏┓┓┏  ┏┓┳┓┏┓┏┓┳┓ ┏┓
+#    ┣┫┣┫┗┓┣┫━━┃┃┣┫┃┃┣ ┃┃ ┣
+#    ┻┛┛┗┗┛┛┗  ┣┛┛┗┗┛┻ ┻┗┛┗┛
 #
 
-# If not running interactively, don't do anything
-[[ $- != *i* ]] && return
-
-# Load my modular bashrc
-if [ -f "$HOME/.config/bashrc/rc" ]; then
-  . "$HOME/.config/bashrc/rc"
-fi
+# Source bashrc
+[[ -f ~/.bashrc ]] && . ~/.bashrc
--- fish/conf.d/30-colors.fish
+++ fish/conf.d/30-colors.fish
@@ -3,17 +3,17 @@
 #    ┗┛┗┛┗┛┗┛┛┗┗┛
 #
 
-# TokyoNight Color Palette
-set -l foreground c0caf5
-set -l selection 283457
-set -l comment 565f89
-set -l red f7768e
-set -l orange ff9e64
-set -l yellow e0af68
-set -l green 9ece6a
-set -l purple 9d7cd8
-set -l cyan 7dcfff
-set -l pink bb9af7
+# solarized-osaka Color Palette
+set -l foreground 839395
+set -l selection 1A6397
+set -l base01 576D74
+set -l red DB302D
+set -l orange C94C16
+set -l yellow B28500
+set -l green 849900
+set -l blue 46ACF5
+set -l cyan 29A298
+set -l pink D23681
 
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
@@ -22,19 +22,18 @@ set -g fish_color_keyword $pink
 set -g fish_color_quote $yellow
 set -g fish_color_redirection $foreground
 set -g fish_color_end $orange
-set -g fish_color_option $pink
 set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
+set -g fish_color_param $blue
+set -g fish_color_comment $base01
 set -g fish_color_selection --background=$selection
 set -g fish_color_search_match --background=$selection
 set -g fish_color_operator $green
 set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
+set -g fish_color_autosuggestion $base01
 
 # Completion Pager Colors
-set -g fish_pager_color_progress $comment
+set -g fish_pager_color_progress $base01
 set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
+set -g fish_pager_color_description $base01
 set -g fish_pager_color_selected_background --background=$selection

edcb99b8dbd7e0dc3c257fcb748188146f7dbf59
Abhra00 <abhramondal9674@gmail.com>
2025-12-29 19:39:32 +0530

    Added fzf fuzzy serching

--- fish/conf.d/60-keybind.fish
+++ fish/conf.d/60-keybind.fish
@@ -5,3 +5,6 @@
 
 # Ctrl+O for yazi in insert mode
 bind -M insert \co yazicd
+
+# Ctrl+F for browsing through fzf
+bind -M insert \cf fzf_browse
--- fish/functions/transcode-video-4k.fish
+++ fish/functions/transcode-video-4k.fish
@@ -2,7 +2,7 @@
 #     ┃ ┣┫┣┫┃┃┗┓┃ ┃┃┃┃┣ ━━┃┃┃┃┃┣ ┃┃━━┃┃┃┫
 #     ┻ ┛┗┛┗┛┗┗┛┗┛┗┛┻┛┗┛  ┗┛┻┻┛┗┛┗┛  ┗╋┛┗┛
 #
-function transcode-video-4K
+function transcode-video-4k
     set base (path change-extension '' $argv[1])
     ffmpeg -i $argv[1] -c:v libx265 -preset slow -crf 24 -c:a aac -b:a 192k "$base-optimized.mp4"
 end

d40bc558a92f7d97c84861e42a3acf48da237461
Abhra00 <abhramondal9674@gmail.com>
2025-12-30 02:40:34 +0530

    Nah came back to tokyonight

--- fish/conf.d/20-init.fish
+++ fish/conf.d/20-init.fish
@@ -7,6 +7,11 @@ if command -q pyenv
     pyenv init - | source
 end
 
+# Starship
+if command -q starship
+    starship init fish | source
+end
+
 # Zoxide
 if command -q zoxide
     zoxide init fish | source
--- fish/conf.d/30-colors.fish
+++ fish/conf.d/30-colors.fish
@@ -3,37 +3,38 @@
 #    ┗┛┗┛┗┛┗┛┛┗┗┛
 #
 
-# solarized-osaka Color Palette
-set -l foreground 839395
-set -l selection 002C38
-set -l base01 576D74
-set -l red DB302D
-set -l orange C94C16
-set -l yellow B28500
-set -l green 849900
-set -l blue 46ACF5
-set -l baseblue 268BD3
-set -l pink D23681
+# TokyoNight Color Palette
+set -l foreground C0CAF5
+set -l selection 283457
+set -l comment 565F89
+set -l red F7768E
+set -l orange FF9E64
+set -l yellow E0AF68
+set -l green 9ECE6A
+set -l purple 9D7CD8
+set -l cyan 7DCFFF
+set -l pink BB9AF7
 
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
-set -g fish_color_command $baseblue
+set -g fish_color_command $cyan
 set -g fish_color_keyword $pink
 set -g fish_color_quote $yellow
 set -g fish_color_redirection $foreground
 set -g fish_color_end $orange
+set -g fish_color_option $pink
 set -g fish_color_error $red
-set -g fish_color_param $blue
-set -g fish_color_comment $base01
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
 set -g fish_color_selection --background=$selection
 set -g fish_color_search_match --background=$selection
 set -g fish_color_operator $green
 set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $base01
+set -g fish_color_autosuggestion $comment
 
 # Completion Pager Colors
-set -g fish_pager_color_progress $base01
-set -g fish_pager_color_prefix $baseblue
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $base01
+set -g fish_pager_color_description $comment
 set -g fish_pager_color_selected_background --background=$selection

b85c7a4d1488b1ad81db8ce9287b837b428ae186
Abhra00 <abhramondal9674@gmail.com>
2026-01-20 16:38:07 +0530

    [Fish] Make tty colors to Nagi theme

--- fish/conf.d/70-tty-colors.fish
+++ fish/conf.d/70-tty-colors.fish
@@ -4,21 +4,21 @@
 #
 
 if test "$TERM" = "linux"
-    echo -en "\e]P015161E" # COLOR_0
-    echo -en "\e]P1F7768E" # COLOR_1
-    echo -en "\e]P29ECE6A" # COLOR_2
-    echo -en "\e]P3E0AF68" # COLOR_3
-    echo -en "\e]P47AA2F7" # COLOR_4
-    echo -en "\e]P5BB9AF7" # COLOR_5
-    echo -en "\e]P67DCFFF" # COLOR_6
-    echo -en "\e]P7A9B1D6" # COLOR_7
-    echo -en "\e]P8414868" # COLOR_8
-    echo -en "\e]P9FF899D" # COLOR_9
-    echo -en "\e]PA9FE044" # COLOR_10
-    echo -en "\e]PBFABA4A" # COLOR_11
-    echo -en "\e]PC8DB0FF" # COLOR_12
-    echo -en "\e]PDC7A9FF" # COLOR_13
-    echo -en "\e]PEA4DAFF" # COLOR_14
-    echo -en "\e]PFC0CAF5" # COLOR_15
+    echo -en "\e]P0101314" # COLOR_0
+    echo -en "\e]P1d95762" # COLOR_1
+    echo -en "\e]P2a6d98d" # COLOR_2
+    echo -en "\e]P3f38c61" # COLOR_3
+    echo -en "\e]P495b7e6" # COLOR_4
+    echo -en "\e]P5d282d9" # COLOR_5
+    echo -en "\e]P692e2f2" # COLOR_6
+    echo -en "\e]P7d5d7d8" # COLOR_7
+    echo -en "\e]P82c3334" # COLOR_8
+    echo -en "\e]P9e65c68" # COLOR_9
+    echo -en "\e]PAb0e695" # COLOR_10
+    echo -en "\e]PBff9366" # COLOR_11
+    echo -en "\e]PC9ec1f3" # COLOR_12
+    echo -en "\e]PDde89e5" # COLOR_13
+    echo -en "\e]PE98eeff" # COLOR_14
+    echo -en "\e]PFeef1f2" # COLOR_15
     clear
 end

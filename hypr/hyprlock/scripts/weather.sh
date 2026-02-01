c42769253d15f7ea16692f23bb1e97e13545de21
Abhra00 <abhramondal9674@gmail.com>
2026-02-01 13:49:38 +0530

    Updated weather module in hyprlock

--- hypr/hyprlock/scripts/weather.sh
+++ hypr/hyprlock/scripts/weather.sh
@@ -9,7 +9,7 @@ WEATHER_CACHE="$HOME/.cache/.weather_cache"
 [[ ! -f "$WEATHER_CACHE" ]] && echo "No weather data" && exit 1
 
 # Parse weather data
-condition=$(awk 'NR==1 {printf "%s %s\n", $1, $2}' "$WEATHER_CACHE")
+condition=$(awk 'NR==1 {printf "%s %s %s\n", $1, $2, $3}' "$WEATHER_CACHE")
 feels_like=$(awk 'NR==2 {match($0, /Feels like [0-9]+/); temp=substr($0, RSTART, RLENGTH); gsub(/Feels like /, "", temp); print temp}' "$WEATHER_CACHE")
 
 # Output

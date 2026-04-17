1e19b1fb889fd879fda2c7252f014007f51436e5
Abhra00 <abhramondal9674@gmail.com>
2026-04-17 14:55:59 +0530

    Font stack updated

--- hypr/hyprlock/scripts/bat-status.sh
+++ hypr/hyprlock/scripts/bat-status.sh
@@ -1,46 +1,56 @@
 #!/usr/bin/env bash
-############ Variables ############
+# ┳┓┏┓┏┳┓  ┏┓┏┳┓┏┓┏┳┓┳┳┏┓
+# ┣┫┣┫ ┃ ━━┗┓ ┃ ┣┫ ┃ ┃┃┗┓
+# ┻┛┛┗ ┻   ┗┛ ┻ ┛┗ ┻ ┗┛┗┛
+#
+
 enable_battery=false
-battery_charging=false
-####### Check availability ########
+is_discharging=false
+
 for battery in /sys/class/power_supply/*BAT*; do
   if [[ -f "$battery/uevent" ]]; then
     enable_battery=true
-    if [[ $(cat /sys/class/power_supply/*/status | head -1) == "Charging" ]]; then
-      battery_charging=true
+
+    status=$(<"$battery/status")
+    capacity=$(<"$battery/capacity")
+
+    if [[ "$status" == "Discharging" ]]; then
+      is_discharging=true
     fi
+
     break
   fi
 done
-############# Output #############
+
 if [[ $enable_battery == true ]]; then
-  capacity=$(cat /sys/class/power_supply/*/capacity | head -1)
-  # Determine battery icon based on level (battery_0_bar to battery_full)
-  if [[ $capacity -le 5 ]]; then
+  # Battery icon selection
+  if (( capacity <= 5 )); then
     icon_name="battery_android_0"
-  elif [[ $capacity -le 15 ]]; then
+  elif (( capacity <= 15 )); then
     icon_name="battery_android_1"
-  elif [[ $capacity -le 30 ]]; then
+  elif (( capacity <= 30 )); then
     icon_name="battery_android_2"
-  elif [[ $capacity -le 45 ]]; then
+  elif (( capacity <= 45 )); then
     icon_name="battery_android_3"
-  elif [[ $capacity -le 60 ]]; then
+  elif (( capacity <= 60 )); then
     icon_name="battery_android_4"
-  elif [[ $capacity -le 75 ]]; then
+  elif (( capacity <= 75 )); then
     icon_name="battery_android_5"
-  elif [[ $capacity -le 90 ]]; then
+  elif (( capacity <= 90 )); then
     icon_name="battery_android_6"
   else
     icon_name="battery_android_full"
   fi
-  # Add charging indicator if charging
-  if [[ $battery_charging == true ]]; then
-    icon="<span font_family='Material Symbols Rounded' size='16pt' rise='-4500'>battery_android_frame_bolt</span>"
-  else
+
+  # Icon rendering
+  if [[ $is_discharging == true ]]; then
     icon="<span font_family='Material Symbols Rounded' size='16pt' rise='-4500'>$icon_name</span>"
+  else
+    icon="<span font_family='Material Symbols Rounded' size='16pt' rise='-4500'>battery_android_frame_bolt</span>"
   fi
-  # Add color for low battery
-  if [[ $capacity -le 20 && $battery_charging == false ]]; then
+
+  # Low battery warning
+  if (( capacity <= 20 )) && [[ $is_discharging == true ]]; then
     echo "<span foreground='#c06c5c'>$icon $capacity%</span>"
   else
     echo "$icon $capacity%"
